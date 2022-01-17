9c27dac7a2cdc2fabb965efbcfc788baccafb528
Cat Wu <catwu@cats-mbp.lan>
2022-01-16 19:24:51 -0800

    merge

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,8 @@
+# frequencies by letter x position
 from collections import defaultdict
 from itertools import product
 
-import modal
+# import modal
 
 from wordlist import read_wordlist
 from wordbag import wordbag
@@ -64,7 +65,7 @@ def all_masks(guess):
             masks.append(m)
     return masks
 
-@modal.function
+# @modal.function
 def cut_score(guess):
     scores = []
     for mask in all_masks(guess):
@@ -75,11 +76,11 @@ def cut_score(guess):
 
 # this could take a while
 cut_scored = []
-with modal.run():
-    scored = cut_score.map(all_words)
-    for score, word in scored:
-        all_scores[word]['cut'] = score
-        cut_scored.append((score, word))
+# with modal.run():
+scored = cut_score.map(all_words)
+for score, word in scored:
+    all_scores[word]['cut'] = score
+    cut_scored.append((score, word))
         
 for word in all_words:
     score = cut_score(word)
@@ -101,3 +102,57 @@ print('\n')
 print('Top ten words by cut score:')
 for score, word in list(sorted(cut_scored))[:10]:
     print(f'{word} ({score}, {all_scores[word]["absolute"]}, {all_scores[word]["positional"]})') 
+def freq_by_position():
+    all_words = read_wordlist('wordlist')
+    common_words = all_words[:all_words.index('aahed')]
+
+    frequencies = defaultdict(int)
+    positions = defaultdict(lambda: [0,0,0,0,0])
+    for word in common_words:
+        for i in range(len(word)):
+            l = word[i]
+            frequencies[l] += 1
+            positions[l][i] += 1
+
+    lines = []
+    for l in 'abcdefghijklmnopqrstuvwxyz':
+        freq = int(frequencies[l] / 26)
+        freqs = '{:>2}'.format(freq)
+        pos = [int(10 * (x / sum(positions[l]))) for x in positions[l]]
+        poss = ' '.join([str(x) for x in pos])
+        lines.append((frequencies[l], f'{l}: {freqs} | {poss}'))
+
+    for f, l in reversed(sorted(lines)):
+        print(l)
+
+    scored = []
+    for word in all_words:
+        score = 0
+        for i in range(len(word)):
+            score += positions[word[i]][i]
+        scored.append((score, word))
+
+    print('\n')
+    print('Top ten words:')
+    for score, word in list(reversed(sorted(scored)))[:10]:
+        print(f'{word} ({score})')
+
+def freq_by_letter():
+    all_words = read_wordlist('wordlist')
+    common_words = all_words[:all_words.index('aahed')]
+
+    frequencies = defaultdict(int)
+    for l in 'abcdefghijklmnopqrstuvwxyz':
+        count = 0
+        for word in common_words:
+            if l in word:
+                count += 1
+        frequencies[l] = count
+
+    print(frequencies)
+
+    sorted_freq = dict(sorted(frequencies.items(), key=lambda item: item[1]))
+    print(sorted_freq)
+    # {'j': 27, 'q': 29, 'z': 35, 'x': 37, 'v': 149, 'w': 194, 'k': 202, 'f': 207, 'b': 267, 'm': 298, 'g': 300, 'p': 346, 'd': 370, 'h': 379, 'y': 417, 'c': 448, 'u': 457, 'n': 550, 's': 618, 'i': 647, 'l': 648, 't': 667, 'o': 673, 'r': 837, 'a': 909, 'e': 1056}
+    # earot -> orate
+freq_by_letter()
--- solve.py
+++ solve.py
@@ -52,22 +52,44 @@ def solve(checker):
     prune, remainder = wordbag(wordlist)
     guesses = 0
     while len(remainder()) > 0:
-        guess = random.choice(remainder())
+        guess = "orate" if guesses == 0 else random.choice(remainder())
         mask = checker(guess)
         prune(guess, mask)
         pp(guess, mask)
         guesses += 1
     return guesses
 
+# guesses = 0
+# successes = 0
+# for _ in range(100):
+#     answer = random.choice(wordlist)
+#     checker = make_game(answer)
+#     g = solve(checker)
+#     if g <= 6:
+#         guesses += g
+#         successes += 1
+#     print(f'Solved {answer} in {g} guesses\n')
+# print(f'Solved {successes} / 100 words')
+# print(f'Average score: {guesses/successes}')
+
+# starting with random word
+# Solved 2067 / 2315 words
+# Average score: 4.597968069666183
+
+# start w/ "orate"
+# Solved 2074 / 2315 words
+# Average score: 4.490356798457087
+
 guesses = 0
 successes = 0
-for _ in range(100):
-    answer = random.choice(wordlist)
-    checker = make_game(answer)
+all_words = read_wordlist('wordlist')
+common_words = all_words[:all_words.index('aahed')]
+for word in common_words:
+    checker = make_game(word)
     g = solve(checker)
     if g <= 6:
         guesses += g
         successes += 1
     print(f'Solved {answer} in {g} guesses\n')
-print(f'Solved {successes} / 100 words')
+print(f'Solved {successes} / {len(common_words)} words')
 print(f'Average score: {guesses/successes}')
